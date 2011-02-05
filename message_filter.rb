1d905f89c6b776d087967aa21fc45e0318a0d56a
yujiorama <yujiorama+github@gmail.com>
2011-02-05 16:35:08 +0900

    安直な実装を Enumerable#any? を使うようにリファクタリングした。

--- message_filter.rb
+++ message_filter.rb
@@ -4,9 +4,6 @@ class MessageFilter
   end
   
   def detect? text
-    @words.each do |word|
-      return true if text.include?(word)
-    end
-    false
+    @words.any? {|w| text.include? w}
   end
 end
