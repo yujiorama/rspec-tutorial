133e53a0e45c7374bcd103aee8e9f54acd98c1c2
yujiorama <yujiorama+github@gmail.com>
2011-02-05 15:43:25 +0900

    example を追加した

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -3,4 +3,8 @@ require 'rspec'
 require 'message_filter'
 
 describe MessageFilter do
+  it "should detect message with NG word" do
+    filter = MessageFilter.new("foo")
+    filter.detect?("hello from foo").should == true
+  end
 end
