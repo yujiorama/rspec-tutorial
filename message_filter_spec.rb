dd885966fe18d2e444a7933a2e10c6f5a03ee6af
yujiorama <yujiorama+github@gmail.com>
2011-03-05 11:45:25 +0900

    initializeでワード渡す形式によりこれくらいが限界かも

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -22,43 +22,46 @@ describe MessageFilter do
     it { should have(2).ng_words }
   end
   
-  context 'with argument "foo" attribute with ignore_case' do
-    subject {
-      m = MessageFilter.new("foo")
-      m.ignore_case = true
-      m
-    }
-    its(:ignore_case) { should be_true }
-    it { should be_detect("hello from foo") }
-    it_should_behave_like 'exercise with argument "foo"'
-    it { should be_detect("hello FOO") }
-    it { should have(1).ng_words }
-  end
+  describe :ignore_case do
+    context 'with argument "foo" when enable ignore_case' do
+      subject {
+        m = MessageFilter.new("foo")
+        m.ignore_case = true
+        m
+      }
+      its(:ignore_case) { should be_true }
+      it { should be_detect("hello from foo") }
+      it_should_behave_like 'exercise with argument "foo"'
+      it { should be_detect("hello FOO") }
+      it { should have(1).ng_words }
+    end
 
-  context 'with argument "FOO" attribute with ignore_case' do
-    subject {
-      m = MessageFilter.new("FOO")
-      m.ignore_case = true
-      m
-    }
-    its(:ignore_case) { should be_true }
-    it { should be_detect("hello from foo") }
-    it_should_behave_like 'exercise with argument "foo"'
-    it { should be_detect("hello FOO") }
-    it { should have(1).ng_words }
-  end
+    context 'with argument "FOO" when enable ignore_case' do
+      subject {
+        m = MessageFilter.new("FOO")
+        m.ignore_case = true
+        m
+      }
+      its(:ignore_case) { should be_true }
+      it { should be_detect("hello from foo") }
+      it_should_behave_like 'exercise with argument "foo"'
+      it { should be_detect("hello FOO") }
+      it { should have(1).ng_words }
+    end
 
-  context 'with argument "foo" attribute with not ignore_case' do
-    subject {
-      m = MessageFilter.new("foo")
-      m.ignore_case = false
-      m
-    }
-    its(:ignore_case) { should be_false }
-    it { should be_detect("hello from foo") }
-    it_should_behave_like 'exercise with argument "foo"'
-    it { should_not be_detect("hello FOO") }
-    it { should have(1).ng_words }
+    context 'with argument "foo" when disable ignore_case' do
+      subject {
+        m = MessageFilter.new("foo")
+        m.ignore_case = false
+        m
+      }
+      its(:ignore_case) { should be_false }
+      it { should be_detect("hello from foo") }
+      it_should_behave_like 'exercise with argument "foo"'
+      it { should_not be_detect("hello FOO") }
+      it { should have(1).ng_words }
+    end
+    
   end
 
 end
